d5e78b28ec1cba5b37e931ee2daf0c22a64b5cb9
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2021-10-18 15:57:44 +0200

    correction

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -11,7 +11,7 @@
             <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:">
                 <use xlink:href="#check-circle-fill" /></svg>
             <div>
-                Aucun devoir n'a étais enregistré
+                Aucun devoir n'a été enregistré
             </div>
         </div>
     @else
